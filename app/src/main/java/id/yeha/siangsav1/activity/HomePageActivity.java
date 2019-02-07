dd8696419f30b3e5ee04e069b4cb86be4d6add3c
herfanda <yunus.herfanda@gmail.com>
2019-02-07 21:04:50 +0700

    add function

---
+++ app/src/main/java/id/yeha/siangsav1/activity/HomePageActivity.java
@@ -0,0 +1,16 @@
+package id.yeha.siangsav1.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import id.yeha.siangsav1.R;
+
+public class HomePageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_activity);
+    }
+}
